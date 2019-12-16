f68ee0e298867b2a5d1e86fb369092741b97c70a
arisupriatna14 <arisupriatna703@gmail.com>
2019-12-16 11:02:37 +0700

    initial commit

---
+++ android/app/src/main/java/com/demobridgingmodules/CustomToast/ToastModule.java
@@ -0,0 +1,40 @@
+package com.demobridgingmodules.CustomToast;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+    private static ReactApplicationContext reactContext;
+
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
+    ToastModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "ToastExample";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constant = new HashMap<>();
+        constant.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constant.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constant;
+    }
+
+    @ReactMethod
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
+    }
+}
